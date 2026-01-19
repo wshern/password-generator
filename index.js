4d7c2fad7ef7da526e2b6d3e2d46a9ab49a0ff50
wslim <imeva.ws@gmail.com>
2026-01-20 02:14:27 +0800

    added prevent uncheck all function, removed alert

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 const uppercase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-const numbers = ['0','1','2','3','4','5','6','7','8','9']
-const specialCharacters = ['!', '@','#','$','%','&','_','+','-','=','.', '?']
+const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+const specialCharacters = ['!', '@', '#', '$', '%', '&', '_', '+', '-', '=', '.', '?']
 
 const lowerCheckbox = document.getElementById('lower-checkbox')
 const upperCheckbox = document.getElementById('upper-checkbox')
@@ -20,6 +20,37 @@ function getSecureRandomNumber(max) {
     crypto.getRandomValues(array)
     return array[0] % max
 }
+
+function preventUncheckAll(checkbox) {
+    const checkedCount = checkBoxes.filter(cb => cb.checked).length
+    // OLD WAY: Traditional function
+    // checkBoxes.filter(function(cb) {
+    //     return cb.checked;
+    // })
+
+    /* to count manually how many checkboxes are checked 
+    let checkedCount = 0;
+    for (let i = 0; i < checkBoxes.length; i++) {
+    let currentCheckbox = checkBoxes[i];
+    if (currentCheckbox.checked === true) {
+        checkedCount++;
+    }
+    */
+    if (checkedCount === 0) {
+        checkbox.checked = true
+        return
+    }
+    generatePassword()
+}
+
+checkBoxes.forEach(checkbox => {
+    checkbox.addEventListener('change', function () {
+        let allowGenerate = preventUncheckAll(checkbox)
+        if (allowGenerate) {
+            generatePassword()
+        }
+    })
+})
 
 function generatePassword() {
     const passwordLength = Number(lengthSlider.value)
@@ -42,7 +73,7 @@ function generatePassword() {
         passwordCharacters.push(upperChar)
         includedTypes.push(...uppercase)
     }
-    
+
     if (numberCheckbox.checked) {
         requiredTypes.push('numbers')
         let numberChar = numbers[getSecureRandomNumber(numbers.length)]
@@ -57,17 +88,12 @@ function generatePassword() {
         includedTypes.push(...specialCharacters)
     }
 
-    if (requiredTypes.length === 0) {
-        alert('Please select at least 1 character type.')
-        return
-    }
-
     characterPool.push(...includedTypes)
 
     while (passwordCharacters.length < passwordLength) {
         let randomChar = characterPool[getSecureRandomNumber(characterPool.length)]
         passwordCharacters.push(randomChar)
-    }   
+    }
 
     shufflePassword(passwordCharacters)
     finalPassword.value = passwordCharacters.join('')
@@ -75,35 +101,32 @@ function generatePassword() {
 }
 
 function shufflePassword(arr) {
-        for (let i = (arr.length - 1); i > 0; i--) {
-            let j = getSecureRandomNumber(i + 1)
+    for (let i = (arr.length - 1); i > 0; i--) {
+        let j = getSecureRandomNumber(i + 1)
 
-            let temp = arr[i]
-            arr[i] = arr[j]
-            arr[j] = temp
+        let temp = arr[i]
+        arr[i] = arr[j]
+        arr[j] = temp
 
-            // modern swap method:
-            // [arr[i], arr[j]] = [arr[j], arr[i]]
+        // modern swap method:
+        // [arr[i], arr[j]] = [arr[j], arr[i]]
 
-        }       
-    } 
+    }
+}
 
 
-lengthSlider.addEventListener('input', function() {
+lengthSlider.addEventListener('input', function () {
     lengthValue.textContent = lengthSlider.value
     generatePassword()
 })
 
 refreshBtn.addEventListener('click', generatePassword)
 
-finalPassword.addEventListener('click', function() {
+finalPassword.addEventListener('click', function () {
     navigator.clipboard.writeText(finalPassword.value)
     alert('Password copied to clipboard!')
 })
 
-checkBoxes.forEach(checkbox => {
-    checkbox.addEventListener('change', generatePassword);
-});
 
 // Generate password on initial load
 generatePassword()
